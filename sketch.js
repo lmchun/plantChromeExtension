830527a70d98e7eebe6ea061fe1ee3f022527bba
lmchun <lmchun@sbcglobal.net>
2023-04-19 11:42:25 -0400

    adding how test note

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 console.log('%cMental health Plants 🌿', 'padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; color: white; background-color: #4158D0; background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);')
+console.log("Make sure to test things by opening chrome://extensions, enable developer mode, then click load unpackaged, select the folder of your extension")
 
 //drawing on the canvas
 // function setup(){
