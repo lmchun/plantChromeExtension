4d6f2bf05c49aaf59bdfb158c251e4657c146286
lmchun <lmchun@sbcglobal.net>
2023-04-19 11:34:32 -0400

    fixing version since it is updated for 2023

--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
-function setup(){
-    let c = createCanvas(400,400);
-    c.position(0,0);
+console.log('%cMental health Plants 🌿', 'padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; color: white; background-color: #4158D0; background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);')
+
+//making kittens replace all imgs on a page
+//make sure to add web accessible resources for any images you want to add -> this is not dynamically done
+let imgs = document.getElementsByTagName('img');
+
+for (imgElt of imgs){
+    console.log(imgElt.src);
 }
 
-function draw(){
-    background(0);
-}
+
+//drawing on the canvas
+// function setup(){
+//     let c = createCanvas(400,400);
+//     c.position(0,0);
+// }
+
+// function draw(){
+//     background(0);
+// }
